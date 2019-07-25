b2d10047eab225712e58361934588c67606c99d5
Gustavo Hernandez <gushdezh@hotmail.com>
2019-07-25 11:21:35 -0500

    Tercer commit|modificacion de dB| identacion

--- sistema.sql
+++ sistema.sql
@@ -1,4 +1,10 @@
-CREATE database sistema; create table if not exists datos ( id int not null primary key,
-       nombre varchar (25),
-       dirrecion varchar(25),
-       telefono varchar(15) ) Engine=InnoDB default charset=latin1; 
+CREATE DATABASE IF NOT EXISTS sistema;
+
+CREATE TABLE IF NOT EXISTS datos 
+( 
+    id int(10) not null,
+    nombre varchar (25),
+    dirrecion varchar(25),
+    telefono varchar(15),
+    PRIMARY KEY (id) 
+) Engine=InnoDB default charset=latin1; 
